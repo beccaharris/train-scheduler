da65bb298c3ce34dd5c2de132be528e1aa4565f8
Becca Harris <beccalynharris@gmail.com>
2018-03-17 00:42:04 -0600

    get variables from firebase and calculate minutes until next train

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -12,10 +12,10 @@ $(document).ready(function() {
     messagingSenderId: "921252828224"
   };
   firebase.initializeApp(config);
-  // Variables // 
-  // ========= //
+
   var database = firebase.database();
 
+  // Function on click of submit (add-train) button // 
   $('#add-train').on("click", function(event) {
     event.preventDefault();
     // Grab user input //
@@ -23,20 +23,38 @@ $(document).ready(function() {
     var trainDestination = $('#train-destination').val().trim();
     var trainTime = $('#train-time').val().trim();
     var trainFrequency = $('#train-frequency').val().trim();
-  
+    // Define the object that's gonna be pushed to Firebase
     var newTrain = {
       trainName: trainName,
       trainDestination: trainDestination,
       trainTime: trainTime,
       trainFrequency: trainFrequency
     };
-  
+    // Clear out input fields //
     var trainName = $('#train-name').val(null);
     var trainDestination = $('#train-destination').val(null);
     var trainTime = $('#train-time').val(null);
     var trainFrequency = $('#train-frequency').val(null);
-  
+    // Push the newTrain object to Firebase db //
     database.ref().push(newTrain)
   })
 
+  // Will run after push of newTrain //
+  database.ref().on('child_added', function(childSnapshot, prevChildKey) {
+    // Store everything into a variable //
+    var trainName = childSnapshot.val().trainName
+    var trainDestination = childSnapshot.val().trainDestination
+    var trainTime = childSnapshot.val().trainTime
+    var trainFrequency = childSnapshot.val().trainFrequency
+    
+    // Get the minutes until the next train // 
+    var trainFrequency = parseInt(trainFrequency)
+    var trainTimeConverted = moment(trainTime, 'HH:mm').subtract(1, 'years');
+    var currentTime = moment();
+    var diffTime = moment().diff(moment(trainTimeConverted), 'minutes');   
+    var tRemainder = diffTime % trainFrequency;
+    var minutesTillTrain = trainFrequency - tRemainder;
+
+    
+  })
 })
